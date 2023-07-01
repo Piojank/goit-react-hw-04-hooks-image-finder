3cf3efda953188c27f8fb8931e085dadc8a8e37d
piojank <p.jankowski931026@gmail.com>
2023-07-01 20:01:55 +0200

    fix app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
