e3df9ecb007c7a17d76be66d650e36aea27e907f
piojank <p.jankowski931026@gmail.com>
2023-07-03 21:32:40 +0200

    PixabayAPI refactoring

--- src/utils/PixabayAPI.js
+++ src/utils/PixabayAPI.js
@@ -1,17 +1,27 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '29712287-5d117bcccbf45424d50c3eb4b';
-const IMAGE_TYPE = 'image_type=photo';
-const IMAGE_SETTINGS = 'orientation=horizontal&safesearch=true';
-const PER_PAGE = 'per_page=12';
+const API = axios.create({
+    baseURL: 'https://pixabay.com/api/',
+    params: {
+        key: '29712287-5d117bcccbf45424d50c3eb4b',
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 12
+    },
+});
 
 export const fetchImages = async (name, galleryPage) => {
-    const FETCH_URL = `${BASE_URL}?key=${API_KEY}&q=${name}&${IMAGE_TYPE}&${IMAGE_SETTINGS}&page=${galleryPage}&${PER_PAGE}`;
-
-    return axios.get(FETCH_URL)
-    .then(response => response.data)
-    .catch(error => {
-        throw error;
+    try {
+        const response = await API.get('', {
+            params: {
+                q: name,
+                page: galleryPage
+        }
     });
+    
+    return response.data;
+    } catch (error) {
+        throw error;
+    };
 };
